2c9b2f7ff331ce2e5106b33ae05e45fe70475042
ypenn21 <yannipeng21@gmail.com>
2024-06-28 19:34:45 +0000

    add readme how to run unit tests

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ source .env.example
 host:8000/
 host:8000/up
 host:8000/up/status
+
+
+run tests:
+
+export PYTHONPATH=$PYTHONPATH:/home/user/python-django/src
--- tests/my_test.py
+++ tests/my_test.py
@@ -1,6 +1,8 @@
 import unittest
-from src.up.utils import generate_random_number
-
+# import sys
+# import os
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+from up.utils import generate_random_number
 
 class UtilsTest(unittest.TestCase):
 
