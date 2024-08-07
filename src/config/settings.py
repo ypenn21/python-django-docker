97dfe3bcc56076f1a6a9f5761770eff660fa271b
yannipeng <yannipeng@google.com>
2024-08-06 18:05:07 -0700

    add db configs

--- src/config/settings.py
+++ src/config/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         "USER": os.getenv("POSTGRES_USER", "postgres"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", "password"),
         "HOST": os.getenv("POSTGRES_HOST", "postgres"),
-        "PORT": os.getenv("POSTGRES_PORT", "5000"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
